46018d5f4ba6b3134004feb1ca94e10ff68b205a
shuzon <d.namu.r21@gmail.com>
2016-12-03 11:08:21 +0900

    rails_practiceのrailsプロジェクト作成コマンドを削除

--- rails_practice.sh
+++ rails_practice.sh
@@ -27,15 +27,3 @@ sudo -E update-alternatives --set c++ /usr/bin/g++
 # sqlite3のインストール
 sudo -E apt-get install -y sqlite3 libsqlite3-dev nodejs nodejs-dev
 
-cd $home_path
-mkdir /home/vagrant/rails
-cd /home/vagrant/rails
-cp /vagrant/Gemfile $home_path/rails/.
-# アプリに必要なgem(rubyパッケージ)のインストール
-/home/vagrant/.rbenv/shims/bundler install --path vendor/bundle --jobs=4
-/home/vagrant/.rbenv/shims/bundler exec rails new -sB rails_practice
-cd /home/vagrant/rails/rails_practice
-cp /vagrant/rails_practice/Gemfile /home/vagrant/rails/rails_practice/.
-/home/vagrant/.rbenv/shims/bundler install
-/home/vagrant/.rbenv/shims/bundler update
-/home/vagrant/.rbenv/shims/bundler exec spring binstub --all
